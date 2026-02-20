69cd3f2ff11a793a88c3cf8d508538e1fcff4027
Matthew <matthew@hmongvaj.de>
2026-02-19 21:44:23 -0600

    completed class Metrics

--- Homework-1/main.py
+++ Homework-1/main.py
@@ -1,7 +1,21 @@
+import pynvml
+import numpy as np
+
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import numpy as np
+from collections import defaultdict
+import pynvml
+from datetime import datetime
+import threading
 
-
+# Copied from Pytorch Tutorial Class
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
@@ -20,6 +34,37 @@ class Net(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+# End of Copied Code from Pytorch Tutorial Class
 
+class Metrics:
+    def __init__(self):
+        self.timestamps = []
+        self.gpuMemUsed = []
+        self.gpuMemUnused = []
+        self.gpuUtilization = []
+        self.startTime = datetime.now()
 
-net = Net()
+        try:
+            pynvml.nvmlInit()
+            self.gpuAvailable = True
+            self.gpuHandle = pynvml.nvmlDeviceGetHandleByIndex(0)
+        except:
+            self.gpuAvailable = False
+    
+    def record(self):
+        timePassed = (datetime.now() - self.startTime).total_seconds()
+        self.timestamps.append(timePassed)
+
+        if torch.cuda.is_available():
+            self.gpuMemUsed.append(torch.cuda.memory_allocated() / 1e6)
+            self.gpuMemUnused.append(torch.cuda.memory_allocated() / 1e6)
+
+            if self.gpuAvailable:
+                try:
+                    util = pynvml.nvmlDeviceGetUtilizationRates(self.gpuHandle)
+                    self.gpuUtilization.append(util)
+                except:
+                    self.gpuUtilization.append(0)
+            else:
+                self.gpuUtilization.append(0)
+        
